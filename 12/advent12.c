fd89e16be8d176bb095b5fdc0e2060ca0ab9e40c
Conor Walsh <conor@conorwalsh.net>
2020-12-06 13:10:21 +0000

    Day 6
    Signed-off-by: Conor Walsh <conor@conorwalsh.net>

---
+++ 12/advent12.c
@@ -0,0 +1,112 @@
+/*
+ * Advent of Code challenge 12
+ *
+ * Conor Walsh 2020
+ *
+ * Input must end in newline or will go into infinite loop
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Max line char array length
+#define MAXCHAR 100
+
+int main(int argc, char **argv) {
+	// Check for file input, error if none
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s <file>\n",argv[0]);
+		exit(1);
+	}
+
+	// Setup file
+	FILE *fp;
+	char str[MAXCHAR];
+	char* filename = argv[1];
+
+	// Open file as read and check exists
+	fp = fopen(filename, "r");
+	if (fp == NULL){
+		printf("Could not open file %s\n",filename);
+		return 1;
+	}
+	// Count lines (needed for array sizes)
+     	int count=0;
+	while (fgets(str, MAXCHAR, fp) != NULL){
+		count++;
+	}
+	fclose(fp);
+
+	// Open file again to lines (no need to check exists)
+	fp = fopen(filename, "r");
+	char *all[count];
+	size_t len = 0;
+	for(int i = 0; i < count; i++){
+		all[i] = NULL;
+		len = 0;
+		getline(&all[i], &len, fp);
+	}
+	fclose(fp);
+
+	// For each line check parameters
+	int ans = 0;
+	for (int d = 0; d<count; d++){
+		// Array to store unique letters seen
+		char ulet[30] = { 0 };
+		char let[200] = { 0 };
+		// Store size of above array
+		int curlet = 0;
+		int ucurlet = 0;
+		// Set initial letter
+		ulet[0] = all[d][0];
+		//Count people
+		int people = 0;
+		// If first character is newline go to next group
+		while (all[d][0]!='\n'){
+			int i = 0;
+			// If character is newline go to next person
+			while(all[d][i]!='\n'){
+				// Check every letter to see if its not in list
+				int found = 0;
+				for(int j = 0; j<=ucurlet; j++){
+					if(all[d][i]==ulet[j]){
+						found=1;
+					}
+				}
+				// If not in list add to list
+				if (found==0){
+					ucurlet++;
+					ulet[ucurlet]=all[d][i];
+				}
+				// Add all letters to let
+				let[curlet]=all[d][i];
+				curlet++;
+				i++;
+			}
+			people++;
+			// Only increment if wont overflow array
+			if (d<count-1)
+				d++;
+		}
+		int tot = 0;
+		// Check if the number of times a letter occurs is equal to the number of people
+		// If so all in group have that letter
+		for ( int i = 0; i<=ucurlet; i++){
+			int found = 0;
+			for ( int j = 0; j<=curlet; j++){
+				if ( ulet[i]==let[j] ){
+					found++;
+				}
+			}
+			if ( found == people ){
+				tot++;
+			}
+		}
+		ans+=tot;
+	}
+
+	// Print answer and exit
+	printf("Sum: %d\n", ans);
+	return 0;
+}
